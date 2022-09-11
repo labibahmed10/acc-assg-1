dee8b42a11d551a8b4005447a4e615df41314cc8
labibahmed10 <labib.ahmed.372@gmail.com>
2022-09-11 16:31:25 +0600

    updated a single user with id and fs

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,7 +8,7 @@ module.exports.getRamdomUser = (req, res) => {
   res.send({ success: true, result: randomPerson });
 };
 
-module.exports.getAllData = (req, res) => {
+module.exports.getAllUser = (req, res) => {
   const limit = Number(req.query?.limit);
   const result = allData.slice(0, limit);
   if (limit) {
@@ -19,18 +19,52 @@ module.exports.getAllData = (req, res) => {
 };
 
 module.exports.saveAUser = (req, res) => {
-  const { id, name, gender, contact, address, photoUrl } = req.body;
+  const { name, gender, contact, address, photoUrl } = req.body;
   console.log(req.body);
-  if (!id || !name || !gender || !contact || !address || !photoUrl) {
-    res.send({
+  if (!name || !gender || !contact || !address || !photoUrl) {
+    return res.send({
       status: 406,
       success: false,
-      result: "Not Acceptable",
+      result: "Please provide all of the necessary data (gender, name, contact, address and photoUrl).",
     });
   } else {
-    allData.push({ id, name, gender, contact, address, photoUrl });
+    allData.push({ id: allData.length + 1, ...req.body });
+    fs.writeFileSync("./data.json", JSON.stringify(allData));
+
     res.send({ status: 200, success: true, result: "New User added Successfully" });
   }
 };
 
-module.exports.updateAUser = (req, res) => {};
+// {
+//   "gender": "male",
+//   "name": "nicn",
+//   "contact": 12332552,
+//   "address": "banglaaaaaaaaaa",
+//   "photoUrl": "https://cdn1.iconfinder.com/data/icons/random-115/24/person-512.png"
+// }
+
+module.exports.updateAUser = (req, res) => {
+  const { id } = req.params;
+  const updateInfo = req.body;
+
+  if (!id || isNaN(id)) {
+    return res.send({ status: 400, success: false, result: "Please provide a valid id you wants to update" });
+  }
+  if (!updateInfo) {
+    return res.send({ status: 400, success: false, result: "Please provide user info" });
+  }
+
+  let updateUser = allData.find((user) => user.id === Number(id));
+  if (!updateUser) return res.send({ status: 404, success: false, result: "No user found on this id" });
+
+  const index = allData.indexOf(updateUser);
+
+  allData[index] = updateUser = {
+    id: Number(id),
+    ...updateUser,
+    ...req.body,
+  };
+
+  fs.writeFileSync("data.json", JSON.stringify(allData));
+  res.send({ status: 200, success: true, result: "User Updated Successfully" });
+};
--- model/user.model.js
+++ model/user.model.js
@@ -6,12 +6,12 @@ const userController = require("../controller/user.controller");
 router.get("/random", userController.getRamdomUser);
 
 // getting all data here
-router.get("/all", userController.getAllData);
+router.get("/all", userController.getAllUser);
 
 // save a single user
 router.post("/save", userController.saveAUser);
 
 // update a single user
-router.patch("/update", userController.updateAUser);
+router.patch("/update/:id", userController.updateAUser);
 
 module.exports = router;
