825f7c4432c4f99029a97d8b090cce3cba64998d
labibahmed10 <labib.ahmed.372@gmail.com>
2022-09-11 12:49:11 +0600

    get a random person api created

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,12 @@
 const allData = require("../data.json");
 
+module.exports.getRamdomUser = (req, res) => {
+  const randomNum = Math.floor(Math.random() * allData.length + 1);
+  const randomPerson = allData.find((user) => user.id === randomNum);
+  console.log(randomPerson);
+  res.send({ success: true, result: randomPerson });
+};
+
 module.exports.getAllData = (req, res) => {
   const limit = Number(req.query?.limit);
   const result = allData.slice(0, limit);
--- model/user.model.js
+++ model/user.model.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller");
 
+// get a single random user
+router.get("/random", userController.getRamdomUser);
+
 // getting all data here
 router.get("/all", userController.getAllData);
 
